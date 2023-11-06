65a0b50bac0d82fae8afc26c03176e82deb8a8ba
Tremendous1192 <tremendous.duck@gmail.com>
2023-11-06 20:52:31 +0900

    5-1 エラーハンドリングの定義

--- ch05/todo/src/main.rs
+++ ch05/todo/src/main.rs
@@ -1,20 +1,23 @@
-// Import the necessary Actix Web modules
-use actix_web::{get, App, HttpResponse, HttpServer};
+use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
 
-// Define a handler for the root path that returns a "Hello world!" response
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum MyError {}
+
+impl ResponseError for MyError {}
+
 #[get("/")]
-async fn index() -> Result<HttpResponse, actix_web::Error> {
+async fn index() -> Result<HttpResponse, MyError> {
     let response_body = "Hello world!";
     Ok(HttpResponse::Ok().body(response_body))
 }
 
-// Define the main asynchronous function for running the Actix Web server
 #[actix_web::main]
 async fn main() -> Result<(), actix_web::Error> {
-    // Create an Actix Web server that listens on all available network interfaces at port 8080
     HttpServer::new(move || App::new().service(index))
-        .bind("0.0.0.0:8080")? // Bind the server to the specified IP address and port
-        .run() // Run the server
-        .await?; // Await the result
-    Ok(()) // Return Ok to indicate successful execution
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await?;
+    Ok(())
 }
