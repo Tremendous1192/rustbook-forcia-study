ce580d8d8a475d780cacd38764c6ec850b716b74
Tremendous1192 <tremendous.duck@gmail.com>
2023-11-07 20:59:32 +0900

    4-1 プロジェクトの準備 P146 クレートを使用しない場合

---
+++ ch04/samplecli/target/debug/deps/samplecli.d
@@ -0,0 +1,5 @@
+C:\Users\treme\source\RustApps\rustbook-forcia-study\ch04\samplecli\target\debug\deps\samplecli.exe: src\main.rs
+
+C:\Users\treme\source\RustApps\rustbook-forcia-study\ch04\samplecli\target\debug\deps\samplecli.d: src\main.rs
+
+src\main.rs:
---
+++ ch04/samplecli/target/debug/samplecli.d
@@ -0,0 +1,1 @@
+C:\Users\treme\source\RustApps\rustbook-forcia-study\ch04\samplecli\target\debug\samplecli.exe: C:\Users\treme\source\RustApps\rustbook-forcia-study\ch04\samplecli\src\main.rs
